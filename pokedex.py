4105d8025baef0035a82ec36892f7b6e15f8383d
yusuke <160513376+masteergg@users.noreply.github.com>
2024-03-14 00:28:16 -0300

    pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -1,26 +1,25 @@
 print('-'*50)
 print('       Bem-vindo a pokedex by Kavin Yusuke    ')
 print('-'*50)
-# Faixa de entra visual
+
 print('Type 1 to add a Pokemon')
 print('Type 2 to remove a Pokemon')
 print('Type 3 to view information about a Pokemon')
 
-# Instruções para o usuário
 
 pokemon_list = []
 
 while True:
     verification = int(input('write a number according to your preference: '))
-# Solicita ao usuario um numero de acordo com sua preferência    
+    
     if verification == 1:
-        # Uma verificação caso a variável verification receba 1 como resposta
+      
         pokemon_name = input("What's your pokemon's name? ")
         pokemon_type = input('what type is this pokemon? ')
 
         pokemon_info = {'name': pokemon_name, 'type': pokemon_type}
         pokemon_list.append(pokemon_info)
-         # Aqui criei um dicionário com as informações do Pokémon e adiciona as informaçôes a lista
+        
         print('Pokemon added successfully')
 
 
